f70e66f0ac44a5cd23977983413312a59500e717
Rob Sessink <rob.sessink@yucca.io>
2015-11-12 21:00:18 +0100

    Initial import

---
+++ src/main/java/io/yucca/microsoft/onedrive/synchronize/LocalFolderImpl.java
@@ -0,0 +1,100 @@
+/**
+ * Copyright 2015 Rob Sessink
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.yucca.microsoft.onedrive.synchronize;
+
+import java.io.IOException;
+import java.nio.file.Path;
+
+import io.yucca.microsoft.onedrive.OneDriveFolder;
+import io.yucca.microsoft.onedrive.resources.Item;
+
+/**
+ * LocalFolder acts as a local replica of a folder stored in OneDrive.
+ * 
+ * @author yucca.io
+ */
+public class LocalFolderImpl extends LocalItemImpl implements LocalFolder {
+
+    private static final long serialVersionUID = 547616355007257412L;
+
+    public static final String FOLDER_ROOT = "root";
+
+    /**
+     * Construct a LocalFolder. If the folder exists the OneDrive metadata
+     * attributes are read,
+     * 
+     * @param folder Path
+     * @param repository LocalDriveRepository
+     * @throws IOException if reading metadata fails
+     */
+    public LocalFolderImpl(Path path, LocalDriveRepository repository)
+        throws IOException {
+        this.name = path.getFileName().toString();
+        this.path = path;
+        this.repository = repository;
+        if (repository.exists(this)) {
+            repository.readMetadata(this);
+        }
+    }
+
+    /**
+     * Construct a LocalFolder based on OneDriveFolder
+     * 
+     * @param path Path
+     * @param folder OneDriveFolder
+     * @param repository LocalDriveRepository
+     */
+    LocalFolderImpl(Path path, OneDriveFolder folder,
+                    LocalDriveRepository repository) {
+        this.path = path;
+        this.repository = repository;
+        relateWith(folder.getItem());
+    }
+
+    @Override
+    public boolean isContentModified(Item item) throws IOException {
+        return ModificationStatus.NEWER.equals(lastModificationStatus(item));
+    }
+
+    @Override
+    public ResourceType type() {
+        return ResourceType.FOLDER;
+    }
+
+    @Override
+    public boolean isLocalRoot() throws IOException {
+        return FOLDER_ROOT.equals(name);
+        // && LocalDrive.ONEDRIVE_ROOT_MARKER.equals(getParentId()));
+    }
+
+    @Override
+    public void update(Item item) throws IOException {
+        relateWith(item);
+        repository.create(this);
+        super.update(item);
+    }
+
+    @Override
+    public Path resolve(String name) {
+        return path.resolve(name);
+    }
+
+    @Override
+    public String toString() {
+        return "LocalFolder: " + getPath() + " id: " + getId();
+    }
+
+}
